ca62b19af7c2ee11a176c9eb9975975700bbcc48
takuya.ogata <ogacan2004@yahoo.co.jp>
2020-08-10 07:10:15 +0000

    add

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,4 @@
 class Task < ApplicationRecord
-    validates :content, presence: true, length: { muximum: 255 }
+    validates :content, presence: true, length: { maximum: 255 }
     validates :status, presance: true, length: { muximum: 10 }
 end
